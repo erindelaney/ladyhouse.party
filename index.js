917507a0d0bd4af65be94981ae99f25e956e5da0
Ian Mathews <ian@redivis.com>
2018-09-29 01:07:43 -0700

    clip clip

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
+const serverErrors = [
+    'Learning',
+    'Machine learning',
+    'Learning to mine',
+    'Machine learned the block chain',
+
+	'Server error -69: VPN access is disallowed',
+    'ERROR 420: internal throttling has occurred. This has been reported.',
+    'ERROR: Fire. Hot fire.'
+]
 
 document.addEventListener('keydown', function(event) {
       if (event.metaKey && ['w', 'r'].indexOf(event.key) > -1){
@@ -6,3 +16,39 @@ document.addEventListener('keydown', function(event) {
       }
 
   }, false);
+
+$(document).ready(function(){
+	clippy.load('Clippy', function(agent){
+	    window.agent = agent;
+		// Do anything with the loaded agent
+		agent.show();
+		// agent.moveTo(100,100);
+		setTimeout(() => {
+            agent.animate();
+			agent.speak(`It's look like you're trying to go to a party!`);
+        }, 5000)
+	});
+
+	document.getElementById('name_input').addEventListener('keypress', () => {
+		agent.moveTo(Math.random() * document.body.clientWidth, Math.random() * document.body.clientHeight);
+    })
+
+	document.getElementById('submit_button').addEventListener('click', () => {
+	    if (!document.getElementById('name_input').value){
+	        window.alert('Please provide your name!');
+	        return;
+        }
+	    if (!document.body.classList.contains('failed')){
+			window.alert(serverErrors[Math.floor(Math.random() * serverErrors.length)])
+
+			document.body.classList.add('submitted');
+		}
+
+	})
+    document.getElementById('fail_button').addEventListener('click', () => {
+		document.body.classList.add('failed');
+		document.getElementById('fail_button').style.transform = 'translateX(100rem)';
+    })
+})
+
+
